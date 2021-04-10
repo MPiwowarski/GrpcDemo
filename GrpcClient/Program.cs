813830ba6067e13549bb0d6053c0e38999a9b964
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2021-04-10 14:56:25 +0100

    customerClient implementation

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -9,13 +9,23 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var input = new HelloRequest { Name = "Marcin" };
+            //var input = new HelloRequest { Name = "Marcin" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
+            var customerClient = new Customer.CustomerClient(channel);
 
-            var reply = await client.SayHelloAsync(input);
+            var clientRequested = new CustomerLookupModel { UserId = 2 };
 
-            Console.WriteLine(reply.Message);
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+
             Console.ReadLine();
         }
     }
